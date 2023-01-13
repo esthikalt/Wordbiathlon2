b88856ef47e8de12c4d5009e0f95744e16dceaac
esthikalt <116064253+esthikalt@users.noreply.github.com>
2023-01-13 09:13:13 +0100

    Third Commit

--- src/app/type-racer/type-racer.component.ts
+++ src/app/type-racer/type-racer.component.ts
@@ -1,4 +1,6 @@
+import { formatCurrency } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'type-racer',
@@ -10,6 +12,7 @@ export class TypeRacerComponent{
   title = '';
   test = '';
   text = 'Funktioniert das eigentlich?';
+  x = '';
 
   myArray:string[] = [];
   counter = 0;
@@ -18,13 +21,15 @@ export class TypeRacerComponent{
     this.myArray = this.text.split(' ');
   }
 
-  onEnter(x:string){
-    if(x === this.myArray[this.counter]){
+  onEnter(form:NgForm){
+    this.x = form.value.title;
+    if(this.x === this.myArray[this.counter]){
       this.test = this.test + ' ' + this.myArray[this.counter];
       this.myArray[this.counter] = "hallo";
       this.counter++;
     } else {
     }
     console.log(this.counter);
+    form.resetForm();
   }
 }
