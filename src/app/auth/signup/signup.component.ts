a3c98387ea9aa9412a42f83611904fce60a4d92b
esthikalt <116064253+esthikalt@users.noreply.github.com>
2023-01-25 16:12:39 +0100

    Update signup.component.ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -17,7 +17,7 @@ export class SignupComponent {
     if(form.invalid){
       return;
     }
-   this.authService.createUser(form.value.email, form.value.password, form.value.name);
+   this.authService.createUser(form.value.email, form.value.password, form.value.uname);
    this._router.navigate(["/"]);
   }
 }
