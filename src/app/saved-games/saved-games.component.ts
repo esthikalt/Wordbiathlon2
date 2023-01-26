e8c740538078fdfb936743d41baa94099002b434
esthikalt <116064253+esthikalt@users.noreply.github.com>
2023-01-26 21:38:25 +0100

    Delete Score implementiert und Datenbank gereinigt

--- src/app/saved-games/saved-games.component.ts
+++ src/app/saved-games/saved-games.component.ts
@@ -37,7 +37,9 @@ export class SavedGamesComponent {
   scores: Score[] = [];
   private scoresSub: Subscription;
 
-
+  onDelete(){
+    this.score.deleteScore();
+  }
 
   ngOnInit(){
     this.score.getScores();
